7e2b8c126137262fea20f30d2b36a57caebfa680
b2jena <bikashjena@yahoo.com>
2020-11-06 21:25:58 +0530

    Update find_unique_xor.cpp

--- find_unique_xor.cpp
+++ find_unique_xor.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 int findUnique(int *, int);
+/*
+XOR a number with itself odd number of times the result is number itself.
+XOR a number even number of times with itself, the result is 0.
+Also XOR with 0 is always the number itself.
+
+XOR is a binary operation, it stands for "exclusive or", that is to say the resulting bit evaluates to one if only exactly one of the bits is set.
+
+This is its function table:
+
+a | b | a ^ b
+--|---|------
+0 | 0 | 0
+0 | 1 | 1
+1 | 0 | 1
+1 | 1 | 0
+
+*/
+
 int findUnique(int *arr, int n) {
 	// Do XOR of all elements and return
         int res = arr[0];
